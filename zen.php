ae3e6d41731f95414a81e8fb49a704163236ea1d
scawful <justin.scofield@outlook.com>
2019-10-29 14:05:31 +0000

    Trying credentials again

--- zen.php
+++ zen.php
@@ -11,7 +11,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
