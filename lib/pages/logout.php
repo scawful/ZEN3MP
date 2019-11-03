8415c367321f2c88f191e33e7512f11e19dca9f6
scawful <justin.scofield@outlook.com>
2019-11-03 15:49:07 +0000

    Called authentication cookie clear routine

--- lib/pages/logout.php
+++ lib/pages/logout.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+//Clear Session
+$_SESSION["user_login"] = "";
 session_destroy();
+
+// clear cookies
+$utils->clearAuthCookie();
+
 header("Location: ../../index.php")
  ?>
