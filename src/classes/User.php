5e1b6930d7545b154355b0934d63e6e7dd462de7
scawful <justin.scofield@outlook.com>
2019-11-07 06:11:11 +0000

    Housekeeping of templates and User class

--- src/classes/User.php
+++ src/classes/User.php
@@ -1,6 +1,6 @@
 <?php
 class User {
-	private $user_obj;
+	private $user;
 	private $spdo;
 
 	public function __construct($user, $spdo){
@@ -50,100 +50,99 @@ class User {
 	}
 
 	public function isClosed() {
-	$username = $this->user['username'];
+		$username = $this->user['username'];
 		$stmt = $this->spdo->prepare('SELECT user_closed FROM users WHERE username = ?');
 		$stmt->execute([$this->getUsername()]);
 		$row = $stmt->fetch();
 
-	if($row['user_closed'] == 'yes')
-	  return true;
-	else
-	  return false;
+		if($row['user_closed'] == 'yes')
+		  return true;
+		else
+		  return false;
 	}
 
 	public function isFriend($username_to_check) {
 	$usernameComma = "," . $username_to_check . ",";
 
-	if(strstr($this->user['friend_list'], $usernameComma) || $username_to_check == $this->user['username']) {
-	  return true;
-	}
-	else {
-	  return false;
-	}
+		if(strstr($this->user['friend_list'], $usernameComma) || $username_to_check == $this->user['username']) {
+		  return true;
+		} else {
+		  return false;
+		}
 	}
 
+	// ==================== Friend Requests ========================
+
 	public function didReceiveRequest($user_from) {
-	$user_to = $this->getUsername();
+		$user_to = $this->getUsername();
 		$stmt = $this->spdo->prepare('SELECT COUNT(*) FROM friend_requests WHERE user_to = ? AND user_from = ?');
 		$stmt->execute([$user_to, $user_from]);
 		$num_rows = $stmt->fetchColumn();
 
-	if($num_rows > 0) {
-	  return true;
-	}
-	else {
-	  return false;
-	}
+		if($num_rows > 0) {
+		  return true;
+		} else {
+		  return false;
+		}
 	}
 
 	public function didSendRequest($user_to) {
-	$user_from = $this->getUsername();
+		$user_from = $this->getUsername();
 		$stmt = $this->spdo->prepare('SELECT COUNT(*) FROM friend_requests WHERE user_to = ? AND user_from = ?');
 		$stmt->execute([$user_to, $user_from]);
 		$num_rows = $stmt->fetchColumn();
 
-	if($num_rows > 0) {
-	  return true;
-	}
-	else {
-	  return false;
-	}
+		if($num_rows > 0) {
+		  return true;
+		} else {
+		  return false;
+		}
 	}
 
 	public function removeFriend($user_to_remove) {
-	$logged_in_user = $this->getUsername();
+		$logged_in_user = $this->getUsername();
 		$stmt = $this->spdo->prepare('SELECT friend_array FROM users WHERE username = ?');
 		$stmt->execute([$user_to_remove]);
 		$row = $stmt->fetch();
-	$friend_array_username = $row['friend_list'];
+		$friend_array_username = $row['friend_list'];
 
-	$new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_list']);
+		$new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_list']);
 		$remove_friend = $this->spdo->prepare('UPDATE users SET friend_list = ? WHERE username = ?');
 		$remove_friend->execute([$new_friend_array, $logged_in_user]);
 
-	$new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
+		$new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
 		$remove_friend = $this->spdo->prepare('UPDATE users SET friend_list = ? WHERE username = ?');
 		$remove_friend->execute([$new_friend_array, $user_to_remove]);
 	}
 
 	public function sendRequest($user_to) {
-	$user_from = $this->getUsername();
+		$user_from = $this->getUsername();
 		$stmt = $this->spdo->prepare('INSERT INTO friend_requests VALUES(0, ?, ?)');
 		$stmt->execute([$user_to, $user_from]);
 	}
 
 	public function getMutualFriends($user_to_check) {
-	  $mutualFriends = 0;
-	  $user_array = $this->user['friend_list'];
-	  $user_array_explode = explode(",", $user_array);
+		$mutualFriends = 0;
+  		$user_array = $this->user['friend_list'];
+  		$user_array_explode = explode(",", $user_array);
 
-			$stmt = $this->spdo->prepare('SELECT friend_list FROM users WHERE username = ?');
-			$stmt->execute([$user_to_check]);
-			$row = $stmt->fetch();
+		$stmt = $this->spdo->prepare('SELECT friend_list FROM users WHERE username = ?');
+		$stmt->execute([$user_to_check]);
+		$row = $stmt->fetch();
 
-	  $user_to_check_array = $row['friend_list'];
-	  $user_to_check_array_explode = explode(",", $user_to_check_array);
+  		$user_to_check_array = $row['friend_list'];
+  		$user_to_check_array_explode = explode(",", $user_to_check_array);
 
-	  foreach($user_array_explode as $i) {
+		foreach($user_array_explode as $i) {
 
-	    foreach($user_to_check_array_explode as $j) {
+			foreach($user_to_check_array_explode as $j) {
 
-	        if($i == $j && $i != "") {
-	            $mutualFriends++;
-	        }
-	    }
-	  }
-	  return $mutualFriends;
+			    if($i == $j && $i != "") {
+			        $mutualFriends++;
+			    }
+			}
+		}
+		return $mutualFriends;
 	}
 
 	public function getUserStyle() {
@@ -215,63 +214,63 @@ class User {
 	}
 
 	public function getUserAboutDetails() {
-			$user_id = $this->user['id'];
-			$stmt = $this->spdo->prepare('SELECT * FROM users_about WHERE user_id = ?');
-			$stmt->execute([$user_id]);
+		$user_id = $this->user['id'];
+		$stmt = $this->spdo->prepare('SELECT * FROM users_about WHERE user_id = ?');
+		$stmt->execute([$user_id]);
 
-			while ($row = $stmt->fetch()) {
+		while ($row = $stmt->fetch()) {
 
-					$userBio = $row['bio'];
-	    			$userWebsite = $row['website'];
-					$userTwitter = $row['twitter'];
-					$userYouTube = $row['youtube'];
-					$userTwitch = $row['twitch'];
-					$userDiscord = $row['discord'];
-					$userGithub = $row['github'];
-					$userSwitch = $row['switch'];
+			$userBio = $row['bio'];
+			$userWebsite = $row['website'];
+			$userTwitter = $row['twitter'];
+			$userYouTube = $row['youtube'];
+			$userTwitch = $row['twitch'];
+			$userDiscord = $row['discord'];
+			$userGithub = $row['github'];
+			$userSwitch = $row['switch'];
 
-					if($userBio != "")
-							echo "<div class='alert alert-primary' role='alert'>$userBio</div>";
+			if($userBio != "")
+					echo "<div class='alert alert-primary' role='alert'>$userBio</div>";
 
-					echo "<ul class='list-group'>";
+			echo "<ul class='list-group'>";
 
-					if($userWebsite != "")
-							echo "<li class='list-group-item'><i class='typcn typcn-attachment btnPurp icon'></i> <a href='http://$userWebsite'>$userWebsite</a></li>";
-					if($userTwitter != "")
-							echo "<li class='list-group-item'><i class='typcn typcn-social-twitter btnPurp icon'></i> <a href='https://twitter.com/$userTwitter'>$userTwitter</a></li>";
-					if($userYouTube != "")
-							echo "<li class='list-group-item'><i class='typcn typcn-social-youtube btnPurp icon'></i> <a href='https://youtube.com/$userYouTube'>$userYouTube</a></li>";
-					if($userTwitch != "")
-							echo "<li class='list-group-item'><i class='fab fa-twitch btnPurp'></i> <a href='https://twitch.tv/$userTwitch'>$userTwitch</a></li>";
-					if($userGithub != "")
-							echo "<li class='list-group-item'><i class='fab fa-github btnPurp'></i> <a href='https://github.com/$userGithub'>$userGithub</a></li>";
-					if($userSwitch != "")
-							echo "<li class='list-group-item'><i class='fab fa-nintendo-switch btnPurp'></i>$userSwitch</a></li>";
-					if($userDiscord != "")
-							echo "<li class='list-group-item'><i class='fab fa-discord btnPurp'></i> $userDiscord</li>";
+			if($userWebsite != "")
+					echo "<li class='list-group-item'><i class='typcn typcn-attachment btnPurp icon'></i> <a href='http://$userWebsite'>$userWebsite</a></li>";
+			if($userTwitter != "")
+					echo "<li class='list-group-item'><i class='typcn typcn-social-twitter btnPurp icon'></i> <a href='https://twitter.com/$userTwitter'>$userTwitter</a></li>";
+			if($userYouTube != "")
+					echo "<li class='list-group-item'><i class='typcn typcn-social-youtube btnPurp icon'></i> <a href='https://youtube.com/$userYouTube'>$userYouTube</a></li>";
+			if($userTwitch != "")
+					echo "<li class='list-group-item'><i class='fab fa-twitch btnPurp'></i> <a href='https://twitch.tv/$userTwitch'>$userTwitch</a></li>";
+			if($userGithub != "")
+					echo "<li class='list-group-item'><i class='fab fa-github btnPurp'></i> <a href='https://github.com/$userGithub'>$userGithub</a></li>";
+			if($userSwitch != "")
+					echo "<li class='list-group-item'><i class='fab fa-nintendo-switch btnPurp'></i>$userSwitch</a></li>";
+			if($userDiscord != "")
+					echo "<li class='list-group-item'><i class='fab fa-discord btnPurp'></i> $userDiscord</li>";
 
-					echo "</ul>";
+			echo "</ul>";
 
-			}
+		}
 	}
 
 	// ==================== Authentication =========================
 
-	function getTokenByUser($username, $expired) {
+	public function getTokenByUser($username, $expired) {
 		$stmt = $this->spdo->prepare('SELECT * from token_auth where username = ? and is_expired = ?');
 		$stmt->execute([$username, $expired]);
 		$result = $stmt->fetch();
 		return $result;
 	}
 
-	function insertToken($username, $random_password_hash, $random_selector_hash, $expiry_date) {
+	public function insertToken($username, $random_password_hash, $random_selector_hash, $expiry_date) {
 		$stmt = $this->spdo->prepare('INSERT INTO token_auth (username, password_hash, selector_hash, expiry_date) values (?, ?, ?,?)');
 		$stmt->execute([$username, $random_password_hash, $random_selector_hash, $expiry_date]);
 		$result = $stmt->fetch();
 		return $result;
 	}
 
-	function markExpired($tokenID) {
+	public function markExpired($tokenID) {
 		$expired = 1;
 		$stmt = $this->spdo->prepare('UPDATE token_auth SET is_expired = ? WHERE id = ?');
 		$stmt->execute([$expired, $tokenID]);
